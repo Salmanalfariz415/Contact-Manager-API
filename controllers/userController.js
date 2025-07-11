a01d4786dc0b9d21863a2e0b07d069a834db8c8b
salmanalfariz-tech <salmanalfariz332@gmail.com>
2025-07-11 18:32:52 +0530

    feat: Implement user registration logic in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,23 @@
 const asyncHandler = require("express-async-handler");
+const pool = require('../config/database'); 
 const registerUser = asyncHandler(async(req,res)=>{
+    try{
+        const [row] = await pool.query("INSERT INTO user_details (UserName,Email,Password) VALUES (?, ?, ?)",
+        [req.body.Name, req.body.Email, req.body.Password]);
     res.json({
-        message : "User registered successfully"
+        message : "User registered successfully",
+        data:[row]
     })
+    }
+    catch(err){
+        res.status(500).json({
+        message: "Error registering user",
+        error: err.message
+    });
+    throw err;
+
+    }
+
 })
 
 const loginUser = asyncHandler(async(req,res)=>{
